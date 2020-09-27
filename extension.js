d036c87cd848a608ff21412ecfd89c44e2382eb0
zhangrenyang <zhang_renyang@126.com>
2020-09-27 15:29:07 +0800

    fix: do not remove the // in http protocal!

--- extension.js
+++ extension.js
@@ -1,9 +1,9 @@
 const vscode = require('vscode');
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('vscode-plugin-remove-comments.removeComments', function () {
+	let disposable = vscode.commands.registerCommand('vscode-plugin-remove-comments.removeJSComments', function () {
 		vscode.window.activeTextEditor.edit(editBuilder => {
 			let text = vscode.window.activeTextEditor.document.getText();
-			text=text.replace(/((\/\*([\w\W]+?)\*\/)|(\/\/(.(?!"\)))+)|(^\s*(?=\r?$)\n))/gm,'').replace(/(^\s*(?=\r?$)\n)/gm,'').replace(/\\n\\n\?/gm,'');
+			text=text.replace(/(\/\*([\w\W]+?)\*\/)|((?<!:)\/\/(.(?!"\)))+)/gm,'').replace(/^\s*(?=\r?$)\n/gm,'').replace(/\\n\\n\?/gm,'');
 			const end = new vscode.Position(vscode.window.activeTextEditor.document.lineCount + 1, 0);
 			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), text);
 			vscode.commands.executeCommand(`editor.action.formatDocument`);
