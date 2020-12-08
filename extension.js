0b3605c51275abc494912a7fb9b98cf66c5ae796
zhangrenyang <zhang_renyang@126.com>
2020-12-08 11:17:12 +0800

    fix bugs

--- extension.js
+++ extension.js
@@ -3,7 +3,9 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('vscode-plugin-remove-comments.removeJSComments', function () {
 		vscode.window.activeTextEditor.edit(editBuilder => {
 			let text = vscode.window.activeTextEditor.document.getText();
-			text=text.replace(/(\/\*([\w\W]+?)\*\/)|((?<!:)\/\/(.(?!"\)))+)/gm,'').replace(/^\s*(?=\r?$)\n/gm,'').replace(/\\n\\n\?/gm,'');
+			text =text.replace(/^\/\*(.|[\r\n])+?\*\//g,'');//remove multiline comment
+			text =text.replace(/(^|\s)\/\/.+/g,'');//remove Single-Line Comments
+			text =text.replace(/\s+\/\*.+\*\//g,'');//remove multiline comment in line
 			const end = new vscode.Position(vscode.window.activeTextEditor.document.lineCount + 1, 0);
 			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), text);
 			vscode.commands.executeCommand(`editor.action.formatDocument`);
