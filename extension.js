fe89d1d493210eb6cb81fddcf0ccc219c558b24c
zhangrenyang <zhang_renyang@126.com>
2020-12-12 18:17:17 +0800

    1.init

--- extension.js
+++ extension.js
@@ -1,11 +1,15 @@
 const vscode = require('vscode');
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('vscode-plugin-remove-comments.removeJSComments', function () {
+	let disposable = vscode.commands.registerCommand('rm-js-comment.format', function () {
 		vscode.window.activeTextEditor.edit(editBuilder => {
 			let text = vscode.window.activeTextEditor.document.getText();
-			text =text.replace(/^\/\*(.|[\r\n])+?\*\//g,'');//remove multiline comment
-			text =text.replace(/(^|\s)\/\/.+/g,'');//remove Single-Line Comments
-			text =text.replace(/\s+\/\*.+\*\//g,'');//remove multiline comment in line
+			text =text.replace(/\/\*(?!#)([\s\S\r])+?\*\//mg,'');//remove multiline comment
+			text =text.replace(/(^|\s)\/\/.+/mg,'');//remove Single-Line Comments
+			const {replacer={}} = vscode.workspace.getConfiguration("rm-js-comment");
+			for(let oldVal in replacer){//apply use settings replacer
+				text =text.replace(new RegExp(oldVal,'mg'),replacer[oldVal]);
+			}
+			text =text.replace(/(^\s*(?=\r?$)\n)/gm,'').replace(/\\n\\n\?/gm,'');//remove empty lines
 			const end = new vscode.Position(vscode.window.activeTextEditor.document.lineCount + 1, 0);
 			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), text);
 			vscode.commands.executeCommand(`editor.action.formatDocument`);
