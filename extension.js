f776ab6d0e6d50d08f36b53078244a95b7c08cd6
Your Name <you@example.com>
2022-08-13 21:24:50 +0800

    rm-js-comment

--- extension.js
+++ extension.js
@@ -1,23 +1,17 @@
 const vscode = require('vscode');
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('vscode-plugin-remove-comments.removeJSComments', function () {
+	const disposable = vscode.commands.registerCommand('rm-js-comment.removeJsComment', function () {
 		vscode.window.activeTextEditor.edit(editBuilder => {
-			let text = vscode.window.activeTextEditor.document.getText();
-			text =text.replace(/^\/\*(.|[\r\n])+?\*\//g,'');//remove multiline comment
-			text =text.replace(/(^|\s)\/\/.+/g,'');//remove Single-Line Comments
-			text =text.replace(/\s+\/\*.+\*\//g,'');//remove multiline comment in line
+			const text = vscode.window.activeTextEditor.document.getText();
 			const end = new vscode.Position(vscode.window.activeTextEditor.document.lineCount + 1, 0);
 			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), text);
 			vscode.commands.executeCommand(`editor.action.formatDocument`);
-			vscode.window.activeTextEditor;
 		});
 	});
-
 	context.subscriptions.push(disposable);
 }
-exports.activate = activate;
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
